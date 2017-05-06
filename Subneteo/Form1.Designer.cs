15e93c0ba577cb8bdb6c3e1c05aa2b46fd4d8b0b
Ramon1508 <valenzuela_luna@hotmail.com>
2017-05-05 23:14:51 -0600

    Agregar archivos de proyecto.

---
+++ Subneteo/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Subneteo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.TXT_OCT1IP = new System.Windows.Forms.TextBox();
+            this.TXT_OCT2IP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TXT_OCT3IP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TXT_OCT4IP = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TXT_MASCARA = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_hosts_subredes = new System.Windows.Forms.TextBox();
+            this.rb_subredes = new System.Windows.Forms.RadioButton();
+            this.rb_hosts = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TXT_OCT1IP
+            // 
+            this.TXT_OCT1IP.Location = new System.Drawing.Point(24, 30);
+            this.TXT_OCT1IP.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TXT_OCT1IP.MaxLength = 3;
+            this.TXT_OCT1IP.Name = "TXT_OCT1IP";
+            this.TXT_OCT1IP.Size = new System.Drawing.Size(66, 26);
+            this.TXT_OCT1IP.TabIndex = 0;
+            // 
+            // TXT_OCT2IP
+            // 
+            this.TXT_OCT2IP.Location = new System.Drawing.Point(119, 30);
+            this.TXT_OCT2IP.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TXT_OCT2IP.MaxLength = 3;
+            this.TXT_OCT2IP.Name = "TXT_OCT2IP";
+            this.TXT_OCT2IP.Size = new System.Drawing.Size(66, 26);
+            this.TXT_OCT2IP.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = ".";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(187, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ".";
+            // 
+            // TXT_OCT3IP
+            // 
+            this.TXT_OCT3IP.Location = new System.Drawing.Point(213, 30);
+            this.TXT_OCT3IP.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TXT_OCT3IP.MaxLength = 3;
+            this.TXT_OCT3IP.Name = "TXT_OCT3IP";
+            this.TXT_OCT3IP.Size = new System.Drawing.Size(66, 26);
+            this.TXT_OCT3IP.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(281, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 33);
+            this.label3.TabIndex = 6;
+            this.label3.Text = ".";
+            // 
+            // TXT_OCT4IP
+            // 
+            this.TXT_OCT4IP.Location = new System.Drawing.Point(307, 30);
+            this.TXT_OCT4IP.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TXT_OCT4IP.MaxLength = 3;
+            this.TXT_OCT4IP.Name = "TXT_OCT4IP";
+            this.TXT_OCT4IP.Size = new System.Drawing.Size(66, 26);
+            this.TXT_OCT4IP.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(298, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 48);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(424, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.Navy;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Size = new System.Drawing.Size(645, 378);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "No. red";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 40;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "ID de red";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Dir. Broadcast";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Rango";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 260;
+            // 
+            // TXT_MASCARA
+            // 
+            this.TXT_MASCARA.Location = new System.Drawing.Point(24, 202);
+            this.TXT_MASCARA.Name = "TXT_MASCARA";
+            this.TXT_MASCARA.ReadOnly = true;
+            this.TXT_MASCARA.Size = new System.Drawing.Size(349, 26);
+            this.TXT_MASCARA.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_hosts_subredes);
+            this.groupBox1.Controls.Add(this.rb_subredes);
+            this.groupBox1.Controls.Add(this.rb_hosts);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(24, 85);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(349, 88);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Calcular por:";
+            // 
+            // txt_hosts_subredes
+            // 
+            this.txt_hosts_subredes.Location = new System.Drawing.Point(6, 55);
+            this.txt_hosts_subredes.Name = "txt_hosts_subredes";
+            this.txt_hosts_subredes.Size = new System.Drawing.Size(100, 26);
+            this.txt_hosts_subredes.TabIndex = 2;
+            // 
+            // rb_subredes
+            // 
+            this.rb_subredes.AutoSize = true;
+            this.rb_subredes.Location = new System.Drawing.Point(95, 23);
+            this.rb_subredes.Name = "rb_subredes";
+            this.rb_subredes.Size = new System.Drawing.Size(96, 24);
+            this.rb_subredes.TabIndex = 1;
+            this.rb_subredes.Text = "Subredes";
+            this.rb_subredes.UseVisualStyleBackColor = true;
+            // 
+            // rb_hosts
+            // 
+            this.rb_hosts.AutoSize = true;
+            this.rb_hosts.Checked = true;
+            this.rb_hosts.Location = new System.Drawing.Point(6, 25);
+            this.rb_hosts.Name = "rb_hosts";
+            this.rb_hosts.Size = new System.Drawing.Size(69, 24);
+            this.rb_hosts.TabIndex = 0;
+            this.rb_hosts.TabStop = true;
+            this.rb_hosts.Text = "Hosts";
+            this.rb_hosts.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1081, 402);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.TXT_MASCARA);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TXT_OCT4IP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TXT_OCT3IP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TXT_OCT2IP);
+            this.Controls.Add(this.TXT_OCT1IP);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TXT_OCT1IP;
+        private System.Windows.Forms.TextBox TXT_OCT2IP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TXT_OCT3IP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TXT_OCT4IP;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox TXT_MASCARA;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb_subredes;
+        private System.Windows.Forms.RadioButton rb_hosts;
+        private System.Windows.Forms.TextBox txt_hosts_subredes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ Subneteo/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Subneteo
+{
+    public partial class Form1 : Form
+    {
+        String tipoMAC;
+        string ip,mascara;
+        double verificador = 0;
+        string cat1masc, cat2masc, cat3masc, cat4masc;
+        //4294967296
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Msgbox(String cadena)
+        {
+            MessageBox.Show(cadena);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {   //CO
+            if (System.Convert.ToInt16(TXT_OCT1IP.Text) > 255)
+            {
+                Msgbox("Error");
+                TXT_OCT1IP.Focus();
+            }
+            else if (System.Convert.ToInt16(TXT_OCT2IP.Text) > 255)
+            {
+                Msgbox("Error");
+                TXT_OCT2IP.Focus();
+            }
+            else if (System.Convert.ToInt16(TXT_OCT3IP.Text) > 255)
+            {
+                Msgbox("Error");
+                TXT_OCT3IP.Focus();
+            }
+            else if (System.Convert.ToInt16(TXT_OCT4IP.Text) > 255)
+            {
+                Msgbox("Error");
+                TXT_OCT4IP.Focus();
+            }
+            else
+            {
+                ip = convertirbinario(TXT_OCT1IP.Text) + "." + convertirbinario(TXT_OCT2IP.Text) + "." + convertirbinario(TXT_OCT3IP.Text) + "." + convertirbinario(TXT_OCT4IP.Text);
+                crear_mascara();
+                GenerarTipoMAC();
+                TXT_MASCARA.Text = STRbintoint(mascara.Substring(0, 8)).ToString() + "." + STRbintoint(mascara.Substring(9, 8)).ToString() + "." + STRbintoint(mascara.Substring(18, 8)).ToString() + "." + STRbintoint(mascara.Substring(27, 8)).ToString();
+                cat1masc = STRbintoint(mascara.Substring(0, 8)).ToString();
+                cat2masc = STRbintoint(mascara.Substring(9, 8)).ToString();
+                cat3masc = STRbintoint(mascara.Substring(18, 8)).ToString();
+                cat4masc = STRbintoint(mascara.Substring(27, 8)).ToString();
+                GenerarIPS();
+            }
+
+
+        }
+        public void GenerarIPS()
+        {
+            int subs = 2;
+            int hosts = 2;
+            
+            
+
+
+
+            int contadorini;
+            string ipini = TXT_OCT1IP.Text;
+            if (!(cat2masc == "255" || cat2masc == "0"))
+            {
+                subs = Convert.ToInt32(Math.Pow(2, 24 - verificador));
+                contadorini = 256 / Convert.ToInt32(subs);
+            }
+            else if (!(cat3masc == "255" || cat3masc == "0"))
+            {//IPS DE CLASE B
+                hosts = 2;//CAMBIAR ESTA MAL
+                ipini = ipini + "." + TXT_OCT2IP.Text;
+                contadorini = 256 / Convert.ToInt32(hosts);
+                int broadcast = 255;
+                int inicial = 0;
+                int medio1 = 0, medio2 = 0;
+                string IPInsert1 = ipini;
+                string IPInsert2 = ipini;
+                String IPInsert3 = ipini;
+                int i = 0;
+                while (broadcast <= 255)
+                {
+                    medio2 = Convert.ToInt32(medio2) + hosts;
+                    IPInsert1 = IPInsert1 + "." + medio1.ToString() + "." + (inicial).ToString();
+                    IPInsert2 = IPInsert2 + "." + medio2.ToString() + "." + (broadcast).ToString();
+                    IPInsert3 = IPInsert3 + "." + medio1.ToString() + "." + (inicial + 1).ToString() + " - " + IPInsert3 + "." + medio2.ToString() + "." + (broadcast - 1).ToString();
+                    dataGridView1.Rows.Add(i + 1, IPInsert1, IPInsert2, IPInsert3);
+                    medio1 = medio2;
+                }
+            }
+            else //IPS DE CLASE C
+            {
+                if (tipoMAC == "C") //Comprobamos que la IP sea tipo C
+                {
+                    subs = Convert.ToInt32(Math.Pow(2, verificador));
+                    hosts = Convert.ToInt32(Math.Pow(2, 8 - verificador));
+                }
+                if (tipoMAC == "B")
+                {
+                    subs = Convert.ToInt32(Math.Pow(2, verificador));
+                    hosts = Convert.ToInt32(Math.Pow(2, 16 - verificador));
+                }
+                ipini = ipini + "." + TXT_OCT2IP.Text + "." + TXT_OCT3IP.Text;
+                contadorini = 256 / Convert.ToInt32(hosts);
+
+                for (int i = 0; i < hosts; i++)
+                {
+                    int broadcast;
+                    int inicial;
+                    string IPInsert1 = ipini;
+                    string IPInsert2 = ipini;
+                    String IPInsert3 = ipini;
+                    broadcast = (i + 1) * contadorini - 1;
+                    inicial = i * contadorini;
+                    IPInsert1 = IPInsert1 + "." + (inicial).ToString();
+                    IPInsert2 = IPInsert2 + "." + (broadcast).ToString();
+                    IPInsert3 = IPInsert3 + "." + (inicial + 1).ToString() + " - " + IPInsert3 + "." + (broadcast - 1).ToString();
+                    dataGridView1.Rows.Add(i + 1, IPInsert1, IPInsert2, IPInsert3);
+                }
+            }
+        }
+        private String Reemplazo(int posicion, String original, String reemplazo)
+        {//Reemplazo(6,"Archivo","b"); == Archibo
+            String primeracad = "";
+            String segundacad = "";
+            String nuevacad = "";
+            primeracad = original.Substring(0, posicion - 1);
+            segundacad = original.Substring(posicion, original.Length - posicion);
+            nuevacad = primeracad + reemplazo + segundacad;
+            return nuevacad;
+        }
+        private int generarN(int numero)
+        {
+            int i;
+            for (i = 0; i < 50; i++)
+            {
+                double pot = Math.Pow(2,Convert.ToDouble(i));
+                if (numero <= Convert.ToInt32(pot))
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+        
+        private void GenerarTipoMAC()
+        {
+            int primeroct = Convert.ToInt32(TXT_OCT1IP.Text);
+            if (primeroct < 128)
+                tipoMAC = "A";
+            else if (primeroct < 192)
+                tipoMAC = "B";
+            else if (primeroct < 224)
+                tipoMAC = "C";
+            else if (primeroct < 240)
+                tipoMAC = "D";
+            else
+                tipoMAC = "E";
+        }
+        private void TXT_OCT1IP_Leave()
+        {
+
+        }
+        private int completarN(int N)
+        {
+            if (N < 8)
+            {
+                return 8;
+            }
+            else if (N<16)
+            {
+                return 16;
+            }
+            else if (N < 24)
+            {
+                return 24;
+            }
+            return 0;
+        }
+        private int STRbintoint(string cadena)
+        {
+            int numero = 0;
+            for (double i = 0; i < 8; i++)
+            {
+                if (cadena.Substring(Convert.ToInt32(i), 1) == "1")
+                {
+                    numero = numero + Convert.ToInt32(Math.Pow(2, 7 - i));
+                }
+            }
+            return numero;
+        }
+        public void crear_mascara()
+        {
+            mascara = ip;
+            int contador = 0;
+            if (rb_hosts.Checked == true)
+            {
+                int N = generarN(Convert.ToInt32(txt_hosts_subredes.Text) + 2);
+                verificador = N;
+                for (int x = 0; x < completarN(N); x++)
+                {
+                    if (mascara.Substring(34 - contador, 1) != ".")
+                    {
+                        if (contador >= N)
+                        {
+                            mascara = Reemplazo(35 - contador, mascara, "1");
+                        }
+                        else
+                        {
+                            mascara = Reemplazo(35 - contador, mascara, "0");
+                        }
+                    }
+                    contador++;
+                }
+                for (int i = contador; i < 35; i++)
+                {
+                    if (mascara.Substring(34 - contador, 1) != ".")
+                    {
+                        mascara = Reemplazo(35 - contador, mascara, "1");
+                    }
+                    contador++;
+                }
+            }
+            else
+            {
+                int N = generarN(Convert.ToInt32(txt_hosts_subredes.Text));
+                int iteraciones = completarN(N);
+                verificador = N;
+                N = completarN(N) - N;
+                for (int x = 0; x < iteraciones; x++)
+                {
+
+                    if (mascara.Substring(34 - contador, 1) != ".")
+                    {
+                        if (contador >= N)
+                        {
+                            mascara = Reemplazo(35 - contador, mascara, "1");
+                        }
+                        else
+                        {
+                            mascara = Reemplazo(35 - contador, mascara, "0");
+                        }
+                        contador++;
+                    }
+
+                }
+                for (int i = contador; i < 35; i++)
+                {
+                    if (mascara.Substring(34 - contador, 1) != ".")
+                    {
+                        mascara = Reemplazo(35 - contador, mascara, "1");
+                    }
+                    contador++;
+                }
+                
+            }
+        }
+      
+        private string convertirbinario(string octeto)
+        {
+            double valor = Convert.ToDouble(octeto);
+            string ip_temporal = "";
+            while ((Convert.ToInt32(valor) > 0) || (ip_temporal.Length < 8))
+            {
+                int residuo = Convert.ToInt32(valor % 2);
+                ip_temporal = residuo + ip_temporal;
+                valor = Math.Truncate(valor / 2);
+            }
+            return ip_temporal;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        
+        }
+        private int convertir_decimal()
+        {
+           
+
+
+            return 0;
+        }
+    }
+}
